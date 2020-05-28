ffea24718325ae633bef28b5eb4af143ee0ce3a6
eliasanzoategui <Anzooategielias360@gmail.com>
2020-05-28 13:56:36 -0300

    teste de jogo2

--- programas/jogos/teste_de_jogo02.py
+++ programas/jogos/teste_de_jogo02.py
@@ -19,7 +19,7 @@ def save(lista,filename):
         arquivo.write(str(i.cor))
         arquivo.write(')\n')
         arquivo.write('entidades.append(criatura)\n')
-        arquivo.close
+        arquivo.close()
 
 
 
---
+++ save1.py
@@ -0,0 +1,5 @@
+import classe
+import random
+entidades = []
+criatura = classe.Criatura(955,102,99,10)
+entidades.append(criatura)
